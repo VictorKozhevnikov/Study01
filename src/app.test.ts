43566007755eef0e4573719c89771d99c2ed6fa9
Victor Kozhevnikov <victor_kozhevnikov@epam.com>
2022-11-26 13:24:41 +0100

    parse the input and format the output (#3)

--- src/app.test.ts
+++ src/app.test.ts
@@ -3,9 +3,11 @@ import { app } from "./app";
 
 describe("app", () => {
   it("should return hello world", async () =>
+    request(app).get("/").expect(200).expect({ message: "Helllo World" }));
+
+  it("should parse the input", async () =>
     request(app)
-    .get("/")
-    .expect(200)
-    .expect({message: 'Helllo World'})
-    );
+      .get("/route?from=BRU&to=JFK")
+      .expect(200)
+      .expect({ from: "BRU", to: "JFK" }));
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,14 @@
-import {Router} from 'express';
+import { Router } from "express";
 
 export const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Helllo World'});
+routes.get("/", (req, res) => {
+  return res.json({ message: "Helllo World" });
+});
+
+routes.get("/route", (req, res) => {
+  const from = req.query.from;
+  const to = req.query.to;
+
+  return res.json({ from, to });
 });
